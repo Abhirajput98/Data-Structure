ed9fe3b589fbf5d0c4e58ad9ba5a3523c11cb01e
Abhirajput98 <91726488+Abhirajput98@users.noreply.github.com>
2022-11-16 11:03:42 +0530

    trial and testing

---
+++ GLA_SUPER_150/src/LEC_1/a2.java
@@ -0,0 +1,42 @@
+package LEC_1;
+import java.util.Scanner;
+
+public class a2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k= sc.nextInt();
+        int i=1;
+        while(i<=k){
+            int n = sc.nextInt();
+            int sum=0;
+            int sum1=0;
+
+            while(n!=0){
+                int a=n%10;
+                if(a%2==0){
+                    sum+=sum+a;
+                }
+                else{
+                    sum1+=a;
+                }
+                n=n/10;
+
+
+            }
+            if(sum%4==0 || sum1%3==0){
+                System.out.println("yes");
+
+            }
+            else{
+                System.out.println("no");
+            }
+            i++;
+        }
+
+
+
+        }
+
+    }
+
+
